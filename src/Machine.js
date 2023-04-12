faacd19676370bfaab2d5a5962136705323b775f
Atique98 <mohammad.atique@xloopdigital.com>
2023-04-13 00:49:18 +0500

    my four fail test cases

--- src/Machine.js
+++ src/Machine.js
@@ -1,12 +1,36 @@
 module.exports = class Machine {
-    static items = [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}]
+
+    //WRITTEN BY MUHAMMAD ATIQUE
+
+    /*static items = [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}]
+    static amount = 0;
     constructor() {
 
     }
-
-
     seeSelections() { 
-        //console.log(this.items)
-       // return Machine.items;
+        console.log(this.items)
+        return Machine.items;
     }
-};
+    
+    deposit(denomination) {
+        const bills = [10, 20, 50, 100, 500];
+
+        for (let bill of bills) {
+            if (denomination == bill) {
+                Machine.amount += denomination
+                break;
+            }
+        }
+        if (Machine.amount == 0) {
+            return "Denomination not accepted";
+        }
+        return `You have deposited Rs ${Machine.amount}`;
+    }
+
+    selectItem(code){
+        for(let item in Machine.items){
+            if(code==item)return true;
+            return 'The item you selected is unavailable'
+}
+    }
+};*/
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -1,10 +1,14 @@
 const Machine = require('../src/Machine');
 
+//WRITTEN BY MUHAMMAD ATIQUE
+
+//1-As a customer, I want to see that the vending machine has items, so that I can purchase them.
+
 describe('the vending machine', () => {
     it('should have items to purchase', () => {
         // setup
         const machine = new Machine();
-        const expected = [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}];
+        const expected = [{ 'crisps': 100 }, { 'chocolate': 350 }, { 'mints': 70 }];
 
         // exercise
         const actual = machine.seeSelections();
@@ -13,11 +17,53 @@ describe('the vending machine', () => {
         expect(expected).toEqual(actual);
     });
 
-    //it('how much money I have deposited'),()=>{
+//WRITTEN BY MUHAMMAD ATIQUE
+
+//2-As a customer, I want to know how much money I have deposited, so that I know what I can purchase.
+
+it('how much money I have deposited', () => {
+    // arrange
+    const machine = new Machine();
+    const expected = "You have deposited Rs 20";
+    const denomination = 20;
+    // act
+    const actual = machine.deposit(denomination)
+    // assert
+    expect(expected).toEqual(actual)
+}
+)
+
+    //WRITTEN BY MUHAMMAD ATIQUE
+
+    //3-As a customer, I want to add additional money, so that I can use the denominations I have to purchase an item.
+
+    it('I want to add additional money', () => {
         // arrange
-
+        const machine = new Machine();
+        const expected = "You have deposited Rs 70";
+        const denomination = 50;
         // act
-
+        const actual = machine.deposit(denomination)
         // assert
-   // }
+        expect(expected).toEqual(actual)
+    }
+    )
+
+    //WRITTEN BY MUHAMMAD ATIQUE
+
+    //4-As a customer, I want to see a message if my item is unavailable, so that I can make another choice.
+
+    it(' I want to see a message if my item is unavailable', () => {
+        //Arrange
+        const machine = new Machine();
+        const expected = 'The item you selected is unavailable';
+        const code = 1;
+        // act
+        const actual = machine.selectItem(code)
+        //assert
+        expect(expected).toEqual(actual)
+
+
+    }
+    )
 });
